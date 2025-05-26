db75c7c3f8b1154cfedbe5a3128325b67c0d286f
TrieuF <jonnytrieu@live.de>
2025-05-26 10:55:47 +0200

    added Coinbase

--- Blockchain/backend/core/Tx.py
+++ Blockchain/backend/core/Tx.py
@@ -1,10 +1,69 @@
+from sympy import false
+
 from Blockchain.backend.core.Script import Script
+from Blockchain.backend.util.util import int_to_little_endian, bytes_needed, decode_base58, little_endian_to_int
+
+ZERO_HASH = b'\0' * 32
+REWARD = 1
+
+PRIVATE_KEY = 'bananansdadsad'
+MINER_ADDRESS = 'bananana2'
+
+class CoinbaseTx:
+    def __init__(self, BlockHeight):
+        self.BlockHeightInLittleEndian = int_to_little_endian(BlockHeight, bytes_needed(BlockHeight))
+
+        def CoinbaseTransaction(self):
+            prev_tx = ZERO_HASH
+            prev_index = 0xffffffff
+
+            tx_ins = []
+            tx_ins.append(TxIn(prev_tx, prev_index))
+            tx_ins[0].script_sig.cmds.append(self.BlockHeightInLittleEndian)
+
+            tx_outs = []
+            target_amount = REWARD * 1000
+            target_h160 = decode_base58(MINER_ADDRESS)
+            target_script = Script.p2pkh_script(target_h160)
+            tx_outs.append(TxOut(target_amount, target_script))
+
+            return Tx(1, tx_ins, tx_outs, 0)
+
 class Tx:
     def __init__(self, version, tx_ins, tx_outs, locktime):
         self.version = version
         self.tx_ins = tx_ins
         self.tx_outs = tx_outs
         self.locktime = locktime
+
+    def is_coinbase(self):
+        if len(self.tx_ins) != 1:
+            return False
+
+        first_input = self.tx_ins[0]
+
+        if first_input.prev_tx != ZERO_HASH:
+            return False
+
+        if first_input.prev_index != 0xffffffff:
+            return False
+
+        return True
+
+    def to_dict(self):
+
+        if self.is_coinbase():
+            self.tx_ins[0].prev_tx = self.tx_ins[0].prev_tx.hex()
+            self.tx_ins[0].script_sig.cmds[0] = little_endian_to_int(self.tx_ins[0].script_sig.cmd[0])
+            self.tx_ins[0].script_sig = self.tx_ins[0].script_sig.__dict__
+
+        self.tx_ins[0] = self.tx_ins[0].__dict__
+
+        self.tx_outs[0].script_pubkey.cmds[2] = self.tx_outs[0].script_pubkey.cmds[2].hex()
+        self.tx_outs[0].script_pubkey = self.tx_outs[0].script_pubkey.__dict__
+        self.tx_outs[0] = self.tx_outs[0].__dict__
+
+        return self.__dict__
 
 class TxIn:
     def __init__(self, prev_tx, prev_index, script_sig = None, sequence = 0xffffffff):
