1ff9febe16dafe05da8e54dd6ba92dbf568352d5
tobiaspoehnl <tobias1.poehnl@st.oth-regensburg.de>
2025-06-10 14:34:58 +0200

    start of frontend finished

--- Blockchain/backend/core/blockchain.py
+++ Blockchain/backend/core/blockchain.py
@@ -6,6 +6,8 @@ from Blockchain.backend.core.blockheader import BlockHeader
 from Blockchain.backend.util.util import hash256
 from Blockchain.backend.core.database.database import BlockchainDB
 from Blockchain.backend.core.Tx import CoinbaseTx
+from multiprocessing import Process, Manager
+from Blockchain.frontend.run import main
 
 import time
 
@@ -13,8 +15,8 @@ ZERO_HASH = '0' * 64
 VERSION = 1
 
 class Blockchain:
-    def __init__(self):
-        pass
+    def __init__(self, utxos):
+        self.utxos = utxos
 
     def safeInDB(self,block):
         db = BlockchainDB()
@@ -29,6 +31,10 @@ class Blockchain:
         prevBlockHash = ZERO_HASH
         self.addBlock(BlockHeight, prevBlockHash)
 
+    #Keep Track of all unspent Transactions in cache memory for fast retrival
+    def store_uxtos_in_cache(self, Transaction):
+        self.utxos[Transaction.TxId] = Transaction
+
     def addBlock(self, BlockHeight,prevBlockHash):
         timestamp = int(time.time())
         coinbaseInstance = CoinbaseTx(BlockHeight)
@@ -37,6 +43,7 @@ class Blockchain:
         bits = 'ffff001f'
         blockHeader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
         blockHeader.mine()
+        self.store_uxtos_in_cache(coinbaseTx)
         print(f"Block Height {BlockHeight} mined successfully with Nonce value of {blockHeader.nonce}")
         self.safeInDB([Block(BlockHeight,1,blockHeader.__dict__,1,coinbaseTx.to_dict()).__dict__])
 
@@ -51,5 +58,11 @@ class Blockchain:
             self.addBlock(BlockHeight, prevBlockHash)
 
 if __name__ == "__main__":
-    blockchain = Blockchain()
-    blockchain.main()
+    with Manager() as manager:
+        utxos = manager.dict()
+
+        webapp = Process(target = main, args = (utxos,))
+        webapp.start()
+
+        blockchain = Blockchain(utxos)
+        blockchain.main()
