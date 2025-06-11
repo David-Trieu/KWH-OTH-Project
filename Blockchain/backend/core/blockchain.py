d1089bca3651b45b69b8acbe6418c5583224e804
TrieuF <jonnytrieu@live.de>
2025-06-11 15:38:34 +0200

    calculate fee , difficulty target

--- Blockchain/backend/core/blockchain.py
+++ Blockchain/backend/core/blockchain.py
@@ -3,7 +3,7 @@ sys.path.append('/Users/David/Desktop/KWH-OTH-Project')
 
 from Blockchain.backend.core.block import Block
 from Blockchain.backend.core.blockheader import BlockHeader
-from Blockchain.backend.util.util import hash256, merkle_root
+from Blockchain.backend.util.util import hash256, merkle_root, target_to_bits
 from Blockchain.backend.core.database.database import BlockchainDB
 from Blockchain.backend.core.Tx import CoinbaseTx
 from multiprocessing import Process, Manager
@@ -13,11 +13,14 @@ import time
 
 ZERO_HASH = '0' * 64
 VERSION = 1
+INITIAL_TARGET = 0x0000FFFF00000000000000000000000000000000000000000000000000000000
 
 class Blockchain:
     def __init__(self, utxos, MemPool):
         self.utxos = utxos
         self.MemPool = MemPool
+        self.current_target = INITIAL_TARGET
+        self.bits = target_to_bits(INITIAL_TARGET)
 
     def safeInDB(self,block):
         db = BlockchainDB()
@@ -50,6 +53,7 @@ class Blockchain:
                     self.utxos[txId_index[0].hex()] = prev_trans.tx_outs.pop(txId_index[1])
 
     def read_transaction_from_memorypool(self):
+        self.Blocksize = 80
         self.TxIds = []
         self.addTransactionsInBlock = []
         self.remove_spent_transactions = []
@@ -57,6 +61,7 @@ class Blockchain:
         for tx in self.MemPool:
             self.TxIds.append(bytes.fromhex(tx))
             self.addTransactionsInBlock.append(self.MemPool[tx])
+            self.Blocksize += len(self.MemPool[tx].serialize())
 
             for spent in self.MemPool[tx].tx_ins:
                 self.remove_spent_transactions.append([spent.prev_tx, spent.prev_index])
@@ -71,19 +76,38 @@ class Blockchain:
         for tx in self.addTransactionsInBlock:
             self.TxJson.append(tx.to_dict())
 
+    def calculate_fee(self):
+        self.input_amount= 0
+        self.output_amount = 0
+
+        #calc input amount
+        for TxId_index in self.remove_spent_transactions:
+            if TxId_index[0].hex() in self.utxos:
+                self.input_amount += self.utxos[TxId_index[0].hex()].tx_outs[TxId_index[1]].amount
+
+        #calc output
+        for tx in self.addTransactionsInBlock:
+            for tx_out in tx.tx_outs:
+                self.output_amount += tx_out.amount
+
+        self.fee = self.input_amount - self.output_amount
+
     def addBlock(self, BlockHeight,prevBlockHash):
         self.read_transaction_from_memorypool()
+        self.calculate_fee()
         timestamp = int(time.time())
         coinbaseInstance = CoinbaseTx(BlockHeight)
         coinbaseTx = coinbaseInstance.CoinbaseTransaction()
+        self.Blocksize += len(coinbaseTx.serialize())
 
-        self.TxIds.insert(0,bytes.fromhex(coinbaseTx.TxId))
+        coinbaseTx.tx_outs[0].amount = coinbaseTx.tx_outs[0].amount + self.fee
+
+        self.TxIds.insert(0,bytes.fromhex(coinbaseTx.id()))
         self.addTransactionsInBlock.insert(0,coinbaseTx)
 
         merkleRoot = merkle_root(self.TxIds)[::-1].hex()
-        bits = 'ffff001f'
-        blockHeader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
-        blockHeader.mine()
+        blockHeader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, self.bits)
+        blockHeader.mine(self.current_target)
 
         self.remove_spent_Transactions()
         self.remove_transactions_from_memorypool()
@@ -91,7 +115,7 @@ class Blockchain:
         self.convert_to_json()
 
         print(f"Block Height {BlockHeight} mined successfully with Nonce value of {blockHeader.nonce}")
-        self.safeInDB([Block(BlockHeight,1,blockHeader.__dict__,1,self.TxJson).__dict__])
+        self.safeInDB([Block(BlockHeight,self.Blocksize,blockHeader.__dict__,1,self.TxJson).__dict__])
 
     def main(self):
         lastBlock = self.getLastBlock()
